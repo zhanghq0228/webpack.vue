550f96fe64de794d9cfe466dc2e1b7a9fa4787fb
hongqiang <17839217481@163.com>
2019-05-11 15:36:04 +0800

    测试

---
+++ build/webpack.config.practice.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base.js')
+
+let config
+const devServer = {
+    port: 8011,
+    host: '0.0.0.0',
+    overlay: {
+        errors: true,
+    },
+    hot: true
+}
+const defaultPluins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: 'development'
+        }
+    })
+]
+
+config = merge(baseConfig, {
+    entry:path.join(__dirname,'../parctioce/index.js'),
+    devtool: '#cheap-module-eval-source-map',
+    module: {
+        rules: [{
+            test: /\.styl/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                    }
+                },
+                'stylus-loader'
+            ]
+        }]
+    },
+    devServer,
+    plugins: defaultPluins.concat([
+        new webpack.HotModuleReplacementPlugin(),
+    // new webpack.NoEmitOnErrorsPlugin()
+    ])
+})
+
+module.exports = config
